202207c36d9fa77b3d84619e1cf808925a7c3fdd
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-15 20:36:27 +0500

    adding context provider in the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import ContextProvider from '@/utils/Context'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,11 +21,13 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
-          <Navbar />
-          <main className="flex-grow flex items-center justify-center">
-            {children}
-          </main>
-          <Footer />
+          <ContextProvider>
+            <Navbar />
+            <main className="flex-grow flex items-center justify-center">
+              {children}
+            </main>
+            <Footer />
+          </ContextProvider>
         </div>
       </body>
     </html>
