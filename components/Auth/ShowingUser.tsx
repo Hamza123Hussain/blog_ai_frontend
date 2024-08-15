d50178765d165abfb29e45f712e9d9e273eb88b2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-15 21:00:31 +0500

    calling the signout function and clearing the userData

--- components/Auth/ShowingUser.tsx
+++ components/Auth/ShowingUser.tsx
@@ -1,11 +1,30 @@
+import { SignOut } from '@/functions/AUTH/SignOut'
+import { UserContext } from '@/utils/Context'
 import { UserDetails } from '@/utils/UserInterface'
-import React from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useContext } from 'react'
 
 const ShowingUser = ({ User }: { User: UserDetails }) => {
+  const { setUserData } = useContext(UserContext)
+  const Router = useRouter()
+  const ByeBye = async () => {
+    try {
+      const Data = await SignOut()
+      if (Data) {
+        setUserData('')
+        Router.push('/Login')
+      }
+    } catch (error) {
+      console.log('ERROR IN FUNCTION', error)
+    }
+  }
   return (
     <div className=" flex gap-2 items-center   text-white">
       <h6 className=" text-base text-white capitalize">{User.Name}</h6>
-      <button className=" bg-black sm:px-7 px-2 text-red-600 hover:text-black hover:bg-red-500 hover:border-2 hover:border-white rounded-lg">
+      <button
+        onClick={ByeBye}
+        className=" bg-black sm:px-7 px-2 text-red-600 hover:text-black hover:bg-red-500 hover:border-2 hover:border-white rounded-lg"
+      >
         {' '}
         Sign Out
       </button>
