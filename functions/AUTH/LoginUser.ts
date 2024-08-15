d1dc4a8f34276f956bd13eec54c082d1ef45d87a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-15 21:24:55 +0500

    showing alert for incorrect email and password

--- functions/AUTH/LoginUser.ts
+++ functions/AUTH/LoginUser.ts
@@ -3,16 +3,28 @@ import axios from 'axios'
 
 export const LoginUser = async (Inputvalue: any) => {
   const { email, password } = Inputvalue
+
   try {
     const Response = await axios.post(`${APIURL}/api/Users/Login`, {
       email,
       password,
     })
+
     if (Response.status === 200) {
-      //   console.log('API HAS RESPONDED : ', Response.data)
+      // API responded successfully with user data
       return Response.data
     }
-  } catch (error) {
-    console.log('FUNCTION ERROR', error)
+  } catch (error: any) {
+    if (error.response) {
+      if (error.response.status === 401) {
+        alert('Incorrect email or password entered')
+      } else if (error.response.status === 500) {
+        // alert('Incorrect Email or Password Entered.')
+        console.error('Server error:', error.response.data)
+      } else {
+        alert('An unexpected error occurred. Please try again later.')
+        console.error('Unexpected error:', error.response.data)
+      }
+    }
   }
 }
