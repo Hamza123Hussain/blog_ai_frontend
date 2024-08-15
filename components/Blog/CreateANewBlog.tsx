660278a03ba8a4728ad2b0cf9b721cc7cecda160
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-16 03:03:25 +0500

    development on this component will start soon

---
+++ components/Blog/CreateANewBlog.tsx
@@ -0,0 +1,9 @@
+import { UserContext } from '@/utils/Context'
+import React, { useContext } from 'react'
+
+const CreateANewBlog = () => {
+  const { userData } = useContext(UserContext)
+  return <div></div>
+}
+
+export default CreateANewBlog
