66bbf5649ae8fbc6fb4df7d87d4725a6c94e779c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-15 17:26:35 +0500

    layout defined of navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className=" bg-customBg px-2 py-1 text-black flex justify-between">
+      hello
+    </div>
+  )
+}
+
+export default Navbar
