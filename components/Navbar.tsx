fe6cbdbb9d59d26a52b18f7c628ee0e89d2ac9c2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-16 03:03:09 +0500

    preventing hydration error

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,33 +2,42 @@
 import { UserContext } from '@/utils/Context'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import ShowingUser from './Auth/ShowingUser'
 import Link from 'next/link'
 
 const Navbar = () => {
   const Router = useRouter()
   const { userData } = useContext(UserContext)
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    // This ensures that `isClient` is only true on the client side
+    setIsClient(true)
+  }, [])
+
+  if (!isClient) return null
+
   return (
-    <div className=" bg-customBg px-2 flex justify-between items-center ">
+    <div className="bg-customBg px-2 flex justify-between items-center">
       <Link href={'/'}>
-        <div className=" flex items-center gap-2 cursor-pointer">
+        <div className="flex items-center gap-2 cursor-pointer">
           <Image
             src={'/Logo.png'}
             alt="Logo"
             width={50}
             height={40}
-            className=" rounded-full"
+            className="rounded-full"
           />
-          <h3 className=" text-base text-white">Thought Flow</h3>
+          <h3 className="text-base text-white">Thought Flow</h3>
         </div>
-      </Link>{' '}
+      </Link>
       {userData ? (
         <ShowingUser User={userData} />
       ) : (
         <button
           onClick={() => Router.push('/login')}
-          className="bg-green-300 px-7 border-white hover:bg-white hover:text-black text-white rounded-lg   "
+          className="bg-green-300 px-7 border-white hover:bg-white hover:text-black text-white rounded-lg"
         >
           Login
         </button>
