53c5b2b36f47285ebb6006f5ac3538a52587a7c5
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-15 19:06:51 +0500

    login function developed and working 100%

---
+++ functions/AUTH/LoginUser.ts
@@ -0,0 +1,17 @@
+import { APIURL } from '@/utils/SignupInterface'
+import axios from 'axios'
+
+export const LoginUser = async (Inputvalue: any) => {
+  const { email, password } = Inputvalue
+  try {
+    const Response = await axios.post(`${APIURL}/api/Users/Login`, {
+      email,
+      password,
+    })
+    if (Response.status === 200) {
+      console.log('API HAS RESPONDED : ', Response.data)
+    }
+  } catch (error) {
+    console.log('FUNCTION ERROR', error)
+  }
+}
