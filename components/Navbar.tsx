0f86f9763217070bdefc6538b00e20efa4a17942
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-16 01:33:43 +0500

    Creating a link to route to home page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,27 +4,30 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import React, { useContext } from 'react'
 import ShowingUser from './Auth/ShowingUser'
+import Link from 'next/link'
 
 const Navbar = () => {
   const Router = useRouter()
   const { userData } = useContext(UserContext)
   return (
     <div className=" bg-customBg px-2 flex justify-between items-center ">
-      <div className=" flex items-center gap-2">
-        <Image
-          src={'/Logo.png'}
-          alt="Logo"
-          width={50}
-          height={40}
-          className=" rounded-full"
-        />
-        <h3 className=" text-base text-white">Thought Flow</h3>
-      </div>{' '}
+      <Link href={'/'}>
+        <div className=" flex items-center gap-2 cursor-pointer">
+          <Image
+            src={'/Logo.png'}
+            alt="Logo"
+            width={50}
+            height={40}
+            className=" rounded-full"
+          />
+          <h3 className=" text-base text-white">Thought Flow</h3>
+        </div>
+      </Link>{' '}
       {userData ? (
         <ShowingUser User={userData} />
       ) : (
         <button
-          onClick={() => Router.push('/Login')}
+          onClick={() => Router.push('/login')}
           className="bg-green-300 px-7 border-white hover:bg-white hover:text-black text-white rounded-lg   "
         >
           Login
