3f8c92892d2001893ad9331f7d8ec8742d250657
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-15 18:42:57 +0500

    Auth components made

---
+++ app/Login/page.tsx
@@ -0,0 +1,12 @@
+import SignIn from '@/components/Auth/SignIn'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+      <SignIn />
+    </>
+  )
+}
+
+export default Login
---
+++ app/Signup/page.tsx
@@ -0,0 +1,13 @@
+import SignUp from '@/components/Auth/Signup'
+import React from 'react'
+
+const Register = () => {
+  return (
+    <>
+      {' '}
+      <SignUp />
+    </>
+  )
+}
+
+export default Register
