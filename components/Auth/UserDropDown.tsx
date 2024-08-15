701fed79a0071c678d7d4ac7f463aa9301229998
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-16 03:04:02 +0500

    using Link instead of Anchor tag

--- components/Auth/UserDropDown.tsx
+++ components/Auth/UserDropDown.tsx
@@ -1,5 +1,6 @@
 import { SignOut } from '@/functions/AUTH/SignOut'
 import { UserContext } from '@/utils/Context'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import React, { useContext } from 'react'
 
@@ -22,9 +23,12 @@ const UserDropDown = ({ setDropdownOpen }: { setDropdownOpen: any }) => {
       onMouseLeave={() => setDropdownOpen(false)}
       className="absolute right-0 mt-2 w-48 bg-white text-black shadow-md rounded-lg z-10"
     >
-      <a href="/my-blogs" className="block px-4 py-2 text-sm hover:bg-gray-100">
+      <Link
+        href="/dashboard"
+        className="block px-4 py-2 text-sm hover:bg-gray-100"
+      >
         My Blogs
-      </a>
+      </Link>
       <button
         onClick={ByeBye}
         className="w-full text-left block px-4 py-2 text-sm bg-red-500 text-black hover:bg-gray-100"
