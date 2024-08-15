e4e24a1dfba611c89a38dfb903d7cdb63824a216
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-15 18:15:34 +0500

    Changed Colors in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className=" bg-customBg px-2 flex justify-between items-center">
+    <div className=" bg-customBg px-2 flex justify-between items-center ">
       <div className=" flex items-center gap-2">
         <Image
           src={'/Logo.png'}
@@ -12,9 +12,7 @@ const Navbar = () => {
           height={40}
           className=" rounded-full"
         />
-        <h3 className=" text-base bg-clip-text text-transparent bg-gradient-to-r from-pink-300 to-pink-500">
-          Thought Flow
-        </h3>
+        <h3 className=" text-base text-white">Thought Flow</h3>
       </div>{' '}
       <button className="bg-green-300 px-7 border-white hover:bg-white hover:text-black text-white rounded-lg   ">
         Login
