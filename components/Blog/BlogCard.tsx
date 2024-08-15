0417f577e42ddf69663aa7513667d93221a58d40
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-16 03:10:59 +0500

    Added the title and changed the color theme a bit

--- components/Blog/BlogCard.tsx
+++ components/Blog/BlogCard.tsx
@@ -3,8 +3,10 @@ import { FaRegCommentDots } from 'react-icons/fa'
 
 const BlogCard = () => {
   return (
-    <div className="flex flex-col w-full max-w-3xl justify-center p-6 border border-gray-300 rounded-lg shadow-lg bg-white ">
-      <div className=" flex flex-col sm:flex-row items-center justify-between">
+    <div className="flex flex-col w-full max-w-3xl justify-center p-6 border border-gray-300 rounded-lg shadow-lg bg-white">
+      <h2 className="text-2xl font-bold text-gray-900 mb-2">Blog Title</h2>
+
+      <div className="flex flex-col sm:flex-row items-center justify-between mb-4">
         <div className="flex gap-3 items-center">
           <img
             src="https://www.brandsynario.com/wp-content/uploads/2024/03/Hania-Amir-2.jpg"
@@ -12,18 +14,17 @@ const BlogCard = () => {
             width={50}
             height={50}
             className="rounded-full border-2 border-gray-200"
-          />{' '}
-          <h6 className="text-lg font-semibold flex items-center gap-2">
+          />
+          <h6 className="text-lg font-semibold flex items-center gap-2 text-gray-800">
             Hamza Hussain
           </h6>
-        </div>{' '}
-        <p className="text-gray-500  ">
-          {' '}
+        </div>
+        <p className="text-gray-500">
           Created At {new Date().toLocaleDateString()}
         </p>
       </div>
 
-      <h4 className="text-md text-justify text-gray-800 mb-4 font-medium">
+      <p className="text-md text-justify text-gray-800 mb-4 font-medium">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid omnis
         similique quod beatae, totam inventore porro odit unde repellendus
         dolore hic ipsa aperiam quae iusto earum sint eaque molestias numquam.
@@ -31,9 +32,9 @@ const BlogCard = () => {
         reiciendis eaque deserunt reprehenderit blanditiis perspiciatis magni
         iste eveniet perferendis at quidem rem sunt repellendus vel, debitis
         aperiam autem quaerat?
-      </h4>
+      </p>
       <div className="flex justify-between items-center">
-        <button className="flex items-center gap-2 py-2 px-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none">
+        <button className="flex items-center gap-2 py-2 px-4 bg-customBg text-white rounded-lg hover:bg-rose-900 focus:outline-none">
           <FaRegCommentDots /> View Comments
         </button>
       </div>
