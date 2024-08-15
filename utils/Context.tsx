ff0b11be5d5e291dc86bf9bac14a16f387d04b3e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-16 03:02:58 +0500

    storing and getting data from local storage

--- utils/Context.tsx
+++ utils/Context.tsx
@@ -1,10 +1,22 @@
 'use client'
-import { createContext, ReactNode, useState } from 'react'
+import { createContext, ReactNode, useEffect, useState } from 'react'
 
 export const UserContext = createContext<any>(null)
 
 const ContextProvider = ({ children }: { children: ReactNode }) => {
-  const [userData, setUserData] = useState<any[]>()
+  const [userData, setUserData] = useState<any>(() => {
+    try {
+      const storedData = localStorage.getItem('userData')
+      return storedData ? JSON.parse(storedData) : [] // Initialize with empty array if no data
+    } catch (error) {
+      console.error('Failed to parse userData from localStorage:', error)
+      return [] // Fallback to empty array if parsing fails
+    }
+  })
+  useEffect(() => {
+    // Save userData to local storage whenever it changes
+    localStorage.setItem('userData', JSON.stringify(userData))
+  }, [userData])
 
   return (
     <UserContext.Provider value={{ userData, setUserData }}>
