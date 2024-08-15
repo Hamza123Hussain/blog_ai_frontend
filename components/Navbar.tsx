f6b3c46bbd1c02db7daedcdd296b590b11fcdfe7
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-15 17:43:18 +0500

    Logo Added in the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className=" bg-customBg px-2 py-1 text-black flex justify-between">
-      hello
+    <div className=" bg-customBg px-2 text-black flex justify-between">
+      <div className=" flex items-center gap-2">
+        <Image
+          src={'/Logo.png'}
+          alt="Logo"
+          width={50}
+          height={50}
+          className=" rounded-full"
+        />
+      </div>
     </div>
   )
 }
