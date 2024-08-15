25946e42e3c1a9c55ae86f7de99bf0894d32cd3c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-16 03:04:46 +0500

    Development of the Dashboard has also begin

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const DashBoard = () => {
+  return (
+    <div className=" flex flex-col justify-start items-start p-4">
+      {' '}
+      <button className=" bg-blue-500 hover:bg-blue-700 text-white px-6 py-2 rounded-lg">
+        Create A New Blog
+      </button>
+    </div>
+  )
+}
+
+export default DashBoard
