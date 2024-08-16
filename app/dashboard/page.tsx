7f74fa8131876e09ee7af182f54d564a25701cc0
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-16 20:47:17 +0500

    shifting data into another componenet

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,10 @@
 'use client'
 import UserBlogs from '@/components/Blog/UserBlogs'
-import { useRouter } from 'next/navigation'
+
 import React from 'react'
 
 const DashBoard = () => {
-  const Router = useRouter()
-  return (
-    <div className=" flex flex-col justify-start items-start p-4 ">
-      {' '}
-      <button
-        onClick={() => Router.push('/Blog/Create')}
-        className=" bg-blue-500 hover:bg-blue-700 text-white px-6 py-2 rounded-lg"
-      >
-        Create A New Blog
-      </button>{' '}
-      <UserBlogs />
-    </div>
-  )
+  return <UserBlogs />
 }
 
 export default DashBoard
