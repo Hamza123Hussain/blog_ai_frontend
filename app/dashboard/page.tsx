f43700c02de3f4c7bf396a953abe750b2552cf97
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-16 05:21:12 +0500

    Routing create btn to createBlog page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,16 @@
+'use client'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const DashBoard = () => {
+  const Router = useRouter()
   return (
     <div className=" flex flex-col justify-start items-start p-4">
       {' '}
-      <button className=" bg-blue-500 hover:bg-blue-700 text-white px-6 py-2 rounded-lg">
+      <button
+        onClick={() => Router.push('/Blog/Create')}
+        className=" bg-blue-500 hover:bg-blue-700 text-white px-6 py-2 rounded-lg"
+      >
         Create A New Blog
       </button>
     </div>
