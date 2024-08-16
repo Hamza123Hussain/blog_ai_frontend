ce332aa732d6c5b99ac4aa49a944c6dcda0887f8
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-16 06:28:56 +0500

    sending email to the create blog api

--- functions/Blog/CreatingABlog.ts
+++ functions/Blog/CreatingABlog.ts
@@ -1,11 +1,17 @@
 import { APIURL } from '@/utils/SignupInterface'
 import axios from 'axios'
 
-export const CreateBlog = async (text: string, Name: string, title: string) => {
+export const CreateBlog = async (
+  text: string,
+  Name: string,
+  title: string,
+  email: string
+) => {
   const Response = await axios.post(`${APIURL}/api/Posts`, {
     text,
     Name,
     title,
+    email,
   })
 
   try {
