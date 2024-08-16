3207ccd38322e2913f00be8ada7aedb61bc12305
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-16 05:39:36 +0500

    Adding Data in the state and then mapping it and showing it on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
+'use client'
 import BlogCard from '@/components/Blog/BlogCard'
-import Image from 'next/image'
+import { GettingAllBlogs } from '@/functions/Blog/GettingAll'
+import { BLOG } from '@/utils/BlogInterface'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [AllBlogs, SetBlogs] = useState<BLOG[]>([])
+  useEffect(() => {
+    const GetBlogs = async () => {
+      const Data = await GettingAllBlogs()
+      if (Data) {
+        SetBlogs(Data)
+      }
+    }
+    GetBlogs()
+  }, [])
   return (
     <div className=" flex flex-col gap-5 py-4 mx-auto ">
-      <BlogCard />
-      <BlogCard />
+      {AllBlogs.map((element) => {
+        return <BlogCard key={element.id} Blog={element} />
+      })}
     </div>
   )
 }
