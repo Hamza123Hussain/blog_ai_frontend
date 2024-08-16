e87ea15e49d1aad0c5290a8ab0bb963d38b4bb08
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-16 05:21:29 +0500

    Blog is being created successfully

--- components/Blog/CreateANewBlog.tsx
+++ components/Blog/CreateANewBlog.tsx
@@ -2,22 +2,29 @@
 import { UserContext } from '@/utils/Context'
 import React, { useContext, useState } from 'react'
 import Image from 'next/image'
+import { CreateBlog } from '@/functions/Blog/CreatingABlog'
+import { useRouter } from 'next/navigation'
+import toast from 'react-hot-toast'
 
 const CreateANewBlog = () => {
   const { userData } = useContext(UserContext)
   const [title, setTitle] = useState('')
   const [text, setText] = useState('')
   const [image, setImage] = useState<File | null>(null)
-
+  const Router = useRouter()
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
       setImage(e.target.files[0])
     }
   }
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
-    // Handle form submission
+    const GetData = await CreateBlog(text, userData.email, title)
+    if (GetData) {
+      toast.success('BLOG HAS BEEN CREATED')
+      Router.push('/')
+    }
   }
 
   return (
@@ -53,7 +60,7 @@ const CreateANewBlog = () => {
             className="p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-dusty-rose"
           />
         </div>
-
+        {/* 
         <div className="flex flex-col gap-2">
           <label htmlFor="image" className="text-lg font-medium text-gray-700">
             Image
@@ -65,11 +72,11 @@ const CreateANewBlog = () => {
             onChange={handleImageChange}
             className="file:border file:border-gray-300 file:rounded-lg file:px-4 file:py-2 file:text-gray-700 file:bg-gray-50 file:hover:bg-gray-100"
           />
-        </div>
+        </div> */}
 
         <button
           type="submit"
-          className="w-full bg-dusty-rose text-white py-3 rounded-lg shadow-md hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-dusty-rose"
+          className="w-full bg-customBg text-white py-3 rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-dusty-rose"
         >
           Submit
         </button>
