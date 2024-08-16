b06980f594d0ff718370cf47b47e87c87b5f3f70
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-17 02:58:07 +0500

    cleaned up componenet and divided into multiple componenets

--- app/Blog/AI/page.tsx
+++ app/Blog/AI/page.tsx
@@ -1,26 +1,31 @@
 'use client'
 import { UserContext } from '@/utils/Context'
 import React, { useContext, useState } from 'react'
-import Image from 'next/image'
 import { CreateBlog } from '@/functions/Blog/CreatingABlog'
 import { useRouter } from 'next/navigation'
 import toast from 'react-hot-toast'
 import { CreateWithAI } from '@/functions/Blog/CallingAI'
+import { BlogCreate } from '@/utils/BlogCreation'
+import CreateBlogFields from '@/functions/Blog/CreateBlogFields'
+import Loader from '@/components/Loader'
 
 const WriteWithAI = () => {
-  const { userData } = useContext(UserContext)
-  const [title, setTitle] = useState('')
-  const [text, setText] = useState('')
-  const [image, setImage] = useState<File | null>(null)
-  const Router = useRouter()
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files[0]) {
-      setImage(e.target.files[0])
-    }
-  }
+  const { userData, loading, setLoading } = useContext(UserContext)
+  const [CreateValue, SetValue] = useState<BlogCreate>({
+    Title: '',
+    Image: null,
+    Text: '',
+  })
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    const GetData = await CreateBlog(text, userData.Name, title, userData.email)
+  const Router = useRouter()
+
+  const handleSubmit = async () => {
+    const GetData = await CreateBlog(
+      CreateValue.Text,
+      userData.Name,
+      CreateValue.Title,
+      userData.email
+    )
     if (GetData) {
       toast.success('BLOG HAS BEEN CREATED')
       Router.push('/')
@@ -28,11 +33,19 @@ const WriteWithAI = () => {
   }
 
   const CallAI = async () => {
-    const Data = await CreateWithAI(title, text)
+    setLoading(true)
+    const Data = await CreateWithAI(CreateValue.Title, CreateValue.Text)
     if (Data) {
-      setTitle(Data.Title)
-      setText(Data.Description)
+      SetValue((element) => ({
+        ...element,
+        Title: Data.Title,
+        Text: Data.Description,
+      }))
+      setLoading(false)
     }
+  }
+  if (loading) {
+    return <Loader />
   }
   return (
     <div className="flex flex-col items-center bg-gray-100 p-8 rounded-lg shadow-lg max-w-3xl mx-auto my-10">
@@ -40,46 +53,7 @@ const WriteWithAI = () => {
         Create a New Blog With AI
       </h1>
       <div className="w-full space-y-6">
-        <div className="flex flex-col gap-2">
-          <label htmlFor="title" className="text-lg font-medium text-gray-700">
-            Title
-          </label>
-          <input
-            id="title"
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="Enter blog title"
-            className="p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-dusty-rose"
-          />
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <label htmlFor="text" className="text-lg font-medium text-gray-700">
-            Text
-          </label>
-          <textarea
-            id="text"
-            rows={6}
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            placeholder="Enter blog content"
-            className="p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-dusty-rose"
-          />
-        </div>
-        {/* 
-        <div className="flex flex-col gap-2">
-          <label htmlFor="image" className="text-lg font-medium text-gray-700">
-            Image
-          </label>
-          <input
-            id="image"
-            type="file"
-            accept="image/*"
-            onChange={handleImageChange}
-            className="file:border file:border-gray-300 file:rounded-lg file:px-4 file:py-2 file:text-gray-700 file:bg-gray-50 file:hover:bg-gray-100"
-          />
-        </div> */}
+        <CreateBlogFields CreateValue={CreateValue} SetValue={SetValue} />
         <div className=" flex gap-2">
           {' '}
           <button
@@ -89,7 +63,7 @@ const WriteWithAI = () => {
             Write With AI
           </button>
           <button
-            onClick={(e: any) => handleSubmit(e)}
+            onClick={handleSubmit}
             className="w-full bg-customBg text-white py-3 rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-dusty-rose"
           >
             Submit
