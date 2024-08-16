22bef48ece41dcbe9d23378d0d801b19f06bee0b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-17 01:12:21 +0500

    making the registeration components come in the center

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <>
+    <div className=" p-3 mt-5 mx-auto">
       <SignIn />
-    </>
+    </div>
   )
 }
 
--- app/Signup/page.tsx
+++ app/Signup/page.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <>
+    <div className=" mx-auto p-3 mt-5">
       {' '}
       <SignUp />
-    </>
+    </div>
   )
 }
 
