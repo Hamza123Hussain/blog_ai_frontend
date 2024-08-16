2edf6c73f319ff4c9a89acf26d878ef2160b9716
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-17 02:16:31 +0500

    changing the state name and adding a ai btn

--- components/Blog/UserBlogs.tsx
+++ components/Blog/UserBlogs.tsx
@@ -7,17 +7,17 @@ import Loader from '../Loader'
 import { useRouter } from 'next/navigation'
 
 const UserBlogs = () => {
-  const { userData, setloading, loading } = useContext(UserContext)
+  const { userData, setLoading, loading } = useContext(UserContext)
   const [UserBlogs, setBlogs] = useState<BLOG[]>([])
   const Router = useRouter()
   useEffect(() => {
     const GetUserBlogs = async () => {
-      setloading(true)
+      setLoading(true)
       const Data = await GettingUserBlogs(userData.email)
       if (Data) {
         setBlogs(Data)
         console.log('API DATA USER :', Data)
-        setloading(false)
+        setLoading(false)
       }
     }
     GetUserBlogs()
@@ -28,12 +28,21 @@ const UserBlogs = () => {
 
   return (
     <div className=" flex flex-col justify-start items-start p-4 ">
-      <button
-        onClick={() => Router.push('/Blog/Create')}
-        className=" bg-blue-500 hover:bg-blue-700 text-white px-6 py-2 rounded-lg"
-      >
-        Create A New Blog
-      </button>{' '}
+      <div className=" flex gap-2">
+        {' '}
+        <button
+          onClick={() => Router.push('/Blog/Create')}
+          className=" bg-blue-500 hover:bg-blue-700 text-white px-6 py-2 rounded-lg"
+        >
+          Create A New Blog
+        </button>{' '}
+        <button
+          onClick={() => Router.push('/Blog/AI')}
+          className=" bg-orange-400 hover:bg-orange-600 text-white px-6 py-2 rounded-lg"
+        >
+          Write Blog With AI
+        </button>{' '}
+      </div>
       <div className=" flex flex-col gap-5 py-4  ">
         {UserBlogs.map((element) => {
           return <UserBlogCard key={element.id} Blog={element} />
