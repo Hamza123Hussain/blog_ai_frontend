4102a8e127c49fcb8d6218510b68e36c7780ac8c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-17 21:25:08 +0500

    using a TEXT prop as component is used in multiple areas

--- components/Auth/FileField.tsx
+++ components/Auth/FileField.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const FileField = ({
   onChange,
+  Text,
 }: {
   onChange: React.ChangeEventHandler<HTMLInputElement>
+  Text: string
 }) => {
   return (
     <div className="bg-transparent border-slate-300 border-2 p-4 rounded-lg shadow-md">
@@ -11,13 +13,14 @@ const FileField = ({
         htmlFor="profile-image"
         className="block text-gray-500 text-lg font-semibold mb-2"
       >
-        Add Profile Image
+        {Text}
       </label>
       <input
         id="profile-image"
         type="file"
         name="Image"
         onChange={onChange}
+        accept=".jpg,.jpeg,.png,.webp" // Restrict file types
         className="block w-full text-sm text-gray-500 file:cursor-pointer file:bg-blue-500 file:text-white file:py-2 file:px-4 file:rounded-lg file:border-0 hover:file:bg-blue-600"
       />
     </div>
