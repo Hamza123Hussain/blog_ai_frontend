0ceb32677288f7104344ca8dd1c5b5b855e8b967
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-17 07:07:07 +0500

    api to ai for generating comments

--- functions/Blog/CallingAI.ts
+++ functions/Blog/CallingAI.ts
@@ -17,3 +17,19 @@ export const CreateWithAI = async (title: String, text: String) => {
     return null
   }
 }
+export const CreateWithAIComment = async (text: String) => {
+  try {
+    const response = await axios.post(`${APIURL}/api/AI/comment`, { text })
+
+    if (response.status === 200) {
+      console.log('API RESPONDED', response.data)
+      return response.data
+    } else {
+      console.error('Error response status:', response.status)
+      return null
+    }
+  } catch (error) {
+    console.error('Error in RemoveBlog function:', error)
+    return null
+  }
+}
