ba6076f602a434d11a0fa9d6bcd9bb368a177a62
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-17 05:19:06 +0500

    giving loader the complete height of the webpage

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div className=" flex  justify-center items-center mx-auto">
+    <div className=" flex  justify-center items-center mx-auto min-h-screen">
       <div className="progress"></div>{' '}
     </div>
   )
