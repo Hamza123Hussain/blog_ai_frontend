72c6f7abdab01f00289a905b39ceb6ef9111427d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-23 19:12:26 +0500

    sending new data to the backend

--- functions/Blog/GettingUserBlogs.ts
+++ functions/Blog/GettingUserBlogs.ts
@@ -1,8 +1,8 @@
 import { APIURL } from '@/utils/SignupInterface'
 import axios from 'axios'
 
-export const GettingUserBlogs = async (email: string) => {
-  const Response = await axios.get(`${APIURL}/api/User/?email=${email}`)
+export const GettingUserBlogs = async (Name: string) => {
+  const Response = await axios.get(`${APIURL}/api/User/?Name=${Name}`)
   try {
     if (Response.status === 200) {
       // console.log('API RESPONSED ', Response.data)
