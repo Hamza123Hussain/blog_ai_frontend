3127e69a75c0f854bfdcee644490678a0ef912c0
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-23 21:08:07 +0500

    gave most space to children

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,9 @@ export default function RootLayout({
         <ContextProvider>
           <div className="flex flex-col min-h-screen">
             <Navbar />
-            <ConditionalLayout>{children}</ConditionalLayout>
+            <div className="flex-1">
+              <ConditionalLayout>{children}</ConditionalLayout>
+            </div>
             <Footer />
           </div>
         </ContextProvider>
