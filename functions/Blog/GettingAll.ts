b0354cbabac5a786d07ade9e09098519d0ebcc70
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-23 19:12:40 +0500

    this has to be commented out

--- functions/Blog/GettingAll.ts
+++ functions/Blog/GettingAll.ts
@@ -5,7 +5,7 @@ export const GettingAllBlogs = async () => {
   const Response = await axios.get(`${APIURL}/api/Posts/GetAll`)
   try {
     if (Response.status === 200) {
-      // console.log('API RESPONSED ', Response.data)
+      console.log('API RESPONSED ', Response.data)
       return Response.data
     }
   } catch (error) {
