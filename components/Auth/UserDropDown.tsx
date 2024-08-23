d6f4552a5fcb05831740caa98eb6b01203bb283a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-24 03:14:14 +0500

    added a new path in the dropdown

--- components/Auth/UserDropDown.tsx
+++ components/Auth/UserDropDown.tsx
@@ -26,15 +26,22 @@ const UserDropDown = ({ setDropdownOpen }: { setDropdownOpen: any }) => {
       className="absolute right-0 mt-2 w-48 bg-white text-black shadow-md rounded-lg z-10 sm:hover:block"
       onClick={() => setDropdownOpen(false)}
     >
+      {' '}
+      <Link
+        href="/Profile"
+        className="block px-4 py-2 text-sm hover:bg-gray-200"
+      >
+        My Profile
+      </Link>
       <Link
         href="/dashboard"
-        className="block px-4 py-2 text-sm hover:bg-gray-100"
+        className="block px-4 py-2 text-sm hover:bg-gray-200"
       >
         My Blogs
       </Link>
       <button
         onClick={ByeBye}
-        className="w-full text-left block px-4 py-2 text-sm bg-red-500 text-black hover:bg-gray-100"
+        className="w-full text-left block px-4 py-2 text-sm bg-red-500 text-black hover:bg-red-100"
       >
         Sign Out
       </button>
