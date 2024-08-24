556fb9194ce1bd858765f4aa0c683d35213be246
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-25 03:11:18 +0500

    changed the loader style

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className=" flex  justify-center items-center mx-auto min-h-screen">
-      <div className="progress"></div>{' '}
+      <div className="loader"></div>
     </div>
   )
 }
