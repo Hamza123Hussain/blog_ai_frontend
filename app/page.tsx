7b6548a812adf4f18a0295f191f40397315423dd
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-25 02:16:42 +0500

    if no blogs then message will be shown

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import Loader from '@/components/Loader'
 import { GettingAllBlogs } from '@/functions/Blog/GettingAll'
 import { BLOG } from '@/utils/BlogInterface'
 import { useEffect, useState } from 'react'
+import { FaRegSadTear } from 'react-icons/fa' // Importing an icon from React Icons
 
 export default function Home() {
   const [AllBlogs, SetBlogs] = useState<BLOG[]>([])
   const [loading, setLoading] = useState(false)
+
   useEffect(() => {
     const GetBlogs = async () => {
       setLoading(true)
@@ -24,15 +26,21 @@ export default function Home() {
     }
     GetBlogs()
   }, [])
+
   if (loading) {
     return <Loader />
   }
 
   return (
     <div className="flex flex-col gap-6 p-4 mx-auto">
-      {AllBlogs.map((element) => (
-        <BlogCard key={element.id} Blog={element} />
-      ))}
+      {AllBlogs.length === 0 ? (
+        <div className="flex flex-col items-center justify-center text-gray-500">
+          <FaRegSadTear size={48} className="mb-4" />
+          <p>No blogs available at the moment. Please check back later.</p>
+        </div>
+      ) : (
+        AllBlogs.map((element) => <BlogCard key={element.id} Blog={element} />)
+      )}
     </div>
   )
 }
